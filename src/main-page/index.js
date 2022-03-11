00f145d634c47ecb74cd5325bd319f352b5e77dd
Troy Dedmon <razor2@cox.net>
2022-03-11 00:24:39 -0600

    Just adding a comment to see if there is potential mistake here

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react';
-import './main-page.css';
-import Header from './header';
+import { useEffect, useState } from "react";
+import "./main-page.css";
+import Header from "./header";
 import SkillList from "./skill-list";
 
 // This is the top-level component of the application.
 // It is a function component, which is typical for React
 function App() {
-
   // state is private data for the component that can survive re-renders
   // the useState function returns an array containing two items
   // the first is the variable that will reflect the value of the state
   // and the second is the function to set it
-  const [allSkills, setAllSkills] = useState([{ name: "Loading...", description: "be patient please"}]);
+  const [allSkills, setAllSkills] = useState([
+    { name: "Loading...", description: "be patient please" },
+  ]);
 
   // useEffect is a hook from the react library that enables us to create side effects when the state of a component changes
   // a hook is a function that takes two parameters, the first is a function
@@ -22,18 +23,20 @@ function App() {
     // This defines an asynchronous function
     const fetchSkills = async () => {
       // The URL here is just a simple service I spun up on Azure, which responds with a JSON object of skills
-      const response = await fetch("https://jsecinterviewwebapiexampleapi.azure-api.net/skills");
+      const response = await fetch(
+        "https://jsecinterviewwebapiexampleapi.azure-api.net/skills"
+      );
       const skills = await response.json();
-      setAllSkills(skills);
-    }; 
+      setAllSkills(skills); //shouldnt this an empty array so it only runs once???
+    };
     // call the function we just defined
     fetchSkills();
   }, [allSkills]);
 
-// The function returns the JSX to be rendered
+  // The function returns the JSX to be rendered
   return (
-    <div className='container'>
-      <Header subtitle="Just for you, my first React app"/>
+    <div className="container">
+      <Header subtitle="Just for you, my first React app" />
       <div className="mt-3">
         <h4>Why do you want Jamey on your team?</h4>
         <p>Select from the list below to find out!</p>
